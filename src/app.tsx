809f980d8afaa3a2e33f735d5a28d65f60ba8917
Guilherme-Dokka <4lves021@gmail.com>
2024-07-11 00:10:09 -0300

    feat: init project

---
+++ src/app.tsx
@@ -0,0 +1,110 @@
+import {
+  MapPin,
+  Calendar,
+  ArrowRight,
+  UserRoundPlus,
+  Settings2,
+} from "lucide-react";
+import { useState } from "react";
+
+export function App() {
+  const [isGuestInputOpen, setIsGuestInputOpen] = useState(false);
+
+  function openGuestInput() {
+    setIsGuestInputOpen(true);
+  }
+
+  function CloseGuestInput() {
+    setIsGuestInputOpen(false);
+  }
+
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-patern bg-no-repeat bg-center">
+        <div className="max-w-3xl w-full px-6 text-center space-y-10">
+          <div className="flex flex-col items-center gap-3">
+            <img src="/logo.svg" alt="Plann.er" />
+            <p className="text-zinc-300 text-lg">
+              Convide seus amigos e planeje sua próxima viagem
+            </p>
+          </div>
+          <div className="space-y-4">
+            <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+              <div className="flex items-center gap-2 flex-1">
+                <MapPin className="size-5 text-zinc-400" />
+                <input
+                  type="text"
+                  placeholder="Para onde você vai?"
+                  className="bg-transparent text-lg placeholder-zinc-400  outline-none flex-1"
+                  disabled={isGuestInputOpen}
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <Calendar className="size-5 text-zinc-400" />
+                <input
+                  type="text"
+                  placeholder="Quando?"
+                  className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
+                  disabled={isGuestInputOpen}
+                />
+              </div>
+
+              <div className="w-px h-6 bg-zinc-800"></div>
+
+              {isGuestInputOpen ? (
+                <button
+                  onClick={CloseGuestInput}
+                  className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700"
+                >
+                  <Settings2 className="size-5 text-zinc-400" />
+                  Alterar local/data
+                  <ArrowRight className="size-5" />
+                </button>
+              ) : (
+                <button
+                  onClick={openGuestInput}
+                  className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+                >
+                  Continuar
+                  <ArrowRight className="size-5" />
+                </button>
+              )}
+            </div>
+
+            {isGuestInputOpen && (
+              <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+                <div className="flex items-center gap-2 flex-1">
+                  <UserRoundPlus className="size-5 text-zinc-400" />
+                  <input
+                    type="text"
+                    placeholder="Quem estara na viagem?"
+                    className="bg-transparent text-lg placeholder-zinc-400  outline-none flex-1"
+                  />
+                </div>
+
+                <div className="w-px h-6 bg-zinc-800"></div>
+
+                <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                  Confirmar viagem
+                  <ArrowRight className="size-5" />
+                </button>
+              </div>
+            )}
+          </div>
+          <p className="text-sm text-zinc-500">
+            Ao planejar sua viagem pela plann.er você automaticamente concoda{" "}
+            <br />
+            com nossos
+            <a href="#" className="text-zinc-300 underline">
+              <span> termos de uso.</span>
+            </a>
+            e{" "}
+            <a href="#" className="text-zinc-300 underline">
+              políticas de privacidade
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
